b22c9c64854a2c8005fb29bb58acd404eb90786b
Kalle Lehto <58829707+kakexd@users.noreply.github.com>
2025-01-14 23:54:48 +0200

    Add files via upload

--- webscrapper.py
+++ webscrapper.py
@@ -3,9 +3,14 @@ import requests
 from bs4 import BeautifulSoup
 
 
-
+# possible meta pick from the website in example: <meta property="og:url" content="https://www.wowhead.com/classic/quest=626/cortellos-riddle">
 
 quests_list = []
+
+# Splitting URL query into proper layer
+quest_src = soup.find('meta property', content='https://www.wowhead.com/classic/quest=')
+quest_id = quest_src.split('/')[4]
+quest_link = f'https://www.wowhead.com/classic/quest={quest_id}'
 
 for i in range(1, 901):
     # url goes like this www.wowhead.com/classic/quest=207/*quest-name#comments to read the comments from the quest's url.
@@ -16,6 +21,7 @@ for i in range(1, 901):
 
     soup = BeautifulSoup(r.html, 'html.parser')
     comment_list = []
+    quests_list = []
 
     # Code here
 
@@ -23,3 +29,4 @@ for i in range(1, 901):
     # End work of the code
     comment = [Comments]
     comment_list.append(comment)
+    # quest_ID = []
